789420bf4321bcc5fe9e61eaa22dcba3e6902823
BranHu <348090150@qq.com>
2017-09-26 22:11:15 +0800

    删除文件

--- myproject3/src/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Todo from './components/todo';
-// import { DatePicker } from 'antd';
-
-ReactDOM.render(
-	<Todo />,
-	document.getElementById('app')
-);
