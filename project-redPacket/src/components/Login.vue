a4f3ffac2964589a9eea5e8bac700518c4b5a82f
BranHu <348090150@qq.com>
2017-09-26 22:07:03 +0800

    小红包

---
+++ project-redPacket/src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <form class="login">
+    <div>
+      <input type="text" name="" placeholder="Username">
+    </div>
+    <div>
+      <input type="text" name="" placeholder="Password">
+    </div>
+    <div>
+      <button type="submit">Log in</button>
+    </div>
+  </form>
+</template>
+<script></script>
+<style></style>
