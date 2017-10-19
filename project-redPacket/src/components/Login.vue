9d3deb581ea7c94c763bcffa83c79b8e5b7148fc
BranHu <348090150@qq.com>
2017-10-19 22:51:02 +0800

    table

--- project-redPacket/src/components/Login.vue
+++ project-redPacket/src/components/Login.vue
@@ -9,15 +9,66 @@
       <li class="file-api">
         <upload></upload>
       </li>
+      <li class="table" style="width:600px">
+        <z-table :columns="columns" :tabledata="tabledata" type="edit"></z-table>
+      </li>
     </ul>
   </div>
 </template>
 <script>
 import Upload from './Upload'
+import ZTable from './Table'
 export default {
   name: 'Index',
   components: {
-    Upload
+    Upload,
+    ZTable
+  },
+  data: function () {
+    return {
+      columns: [
+        {
+          title: '姓名',
+          key: 'name',
+          width: '80px',
+          align: 'right'
+        },
+        {
+          title: '年龄',
+          key: 'age',
+          width: '100px',
+          align: 'center'
+        },
+        {
+          title: '地址',
+          key: 'address',
+          width: '150px',
+          align: 'right'
+        }
+      ],
+      tabledata: [
+        {
+          name: '王小明',
+          age: 18,
+          address: '北京市朝阳区芍药居'
+        },
+        {
+          name: '张小刚',
+          age: 25,
+          address: '北京市海淀区西二旗'
+        },
+        {
+          name: '李小红',
+          age: 30,
+          address: '上海市浦东新区世纪大道'
+        },
+        {
+          name: '周小伟',
+          age: 26,
+          address: '深圳市南山区深南大道'
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ project-redPacket/src/components/Table/Table.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="table">
+    <div class="add-icon"><span>++</span></div>
+    <div class="header-wrapper">
+      <table>
+        <colgroup>
+          <col width="50px"/>
+          <col v-for="(element, index) of columns" :key="index" :width="element.width" />
+        </colgroup>
+        <thead>
+          <tr>
+            <th>序号</th>
+            <th v-for="(element, index) of columns" :key="index" :style="{textAlign: element.align}">{{element.title}}</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+    <div class="body-wrapper">
+      <table>
+        <colgroup>
+          <col width="50px" />
+          <col v-for="(element, index) of columns" :key="index" :width="element.width"/>
+        </colgroup>
+        <tbody>
+          <tr v-for="(tr, index) of tabledata" :key="index">
+            <td>{{index + 1}}</td>
+            <td v-for="(value, index) of tr" :key="index">{{value}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Table',
+  props: {
+    columns: {
+      type: Array
+    },
+    tabledata: {
+      type: Array
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+  div.table {
+    box-sizing: border-box;
+    width: 100%;
+    div.add-icon {
+      width: 100%;
+      overflow: hidden;
+      span {
+        float: right;
+      }
+    }
+  }
+</style>
+
+
+
